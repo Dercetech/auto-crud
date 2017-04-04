cd7daf4d596ba505d3453a47d911551d5dfcfb67
Mercier Jérémie <jeremy.mercier.ext@bpost.be>
2017-04-04 15:02:05 +0200

    Continuous dev branch

--- src/autocrud-handler-generator-factory.js
+++ src/autocrud-handler-generator-factory.js
@@ -1,5 +1,6 @@
 'use strict';
 module.exports = function autoCRUDHAndlerGeneratorFactory(){
+	
 	let autoCRUDHandlerGenerator = require('./autocrud-handler-generator');	
 	return autoCRUDHandlerGenerator
 }
